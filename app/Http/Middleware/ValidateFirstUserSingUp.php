a01cf83c28145cfa43fc49a0dcf8a3e963b2bbc4
Erick Zarate <zaraterick@outlook.com>
2018-05-09 18:08:22 -0500

    Implements middleware first.user.

---
+++ app/Http/Middleware/ValidateFirstUserSingUp.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+use App\User;
+
+use Illuminate\Support\Facades\Auth;
+
+class ValidateFirstUserSingUp
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $usersCount = User::count();
+
+        if ($usersCount > 0 && !Auth::check()) {
+
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
